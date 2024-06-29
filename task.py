037a35cf8806e6555c290685a2fc6baa0ef7b169
Abhishekkannan777R <121169075+Abhishekkannan777R@users.noreply.github.com>
2024-06-29 14:32:35 +0530

    Update task.py

--- task.py
+++ task.py
@@ -1,11 +1,10 @@
 def print_pattern(rows, cols):
-    for c in range(cols+1):
-        # Print the top part of the hexagons
+    for c in range(cols+1):        
         for r in range(rows):
             if c == 0:
                 print("  __      ", end="")
             if r == rows-1 :
-                print()  # New line after top part of the hexagons
+                print() 
         for k in range(rows):
             if c%2==0:
                 if(k == rows-1 ):
@@ -15,11 +14,9 @@ def print_pattern(rows, cols):
             else:
                 print("\ __ /    ",end="")
 
-        # Print the middle part of the hexagons
         
-        # New line after the bottom part of the hexagons
 
-# Example usage
+
 print_pattern(3, 5)
 print()
 print()
